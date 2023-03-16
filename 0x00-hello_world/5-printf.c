fcd00c2b30d0b880d9421adb6a9a40aa9ffa2118
Jonathan Kyule <jonathankyule2@gmail.com>
2023-03-16 16:31:15 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: main - print a string with a new line
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	printf("\" with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
