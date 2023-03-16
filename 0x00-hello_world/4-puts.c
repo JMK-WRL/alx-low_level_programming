56e11ef6fbd8c240be7b5fa887d57bf979c67e7c
Jonathan Kyule <jonathankyule2@gmail.com>
2023-03-16 15:49:52 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints Programming is like building a multilingual puzzle
+ * return/end: returns 0
+ */
+
+int main(void)
+{
+	fputs("Programming is like building a multilingual puzzle");
+	return (0);
+}
