7d70fb4241859b0b95abe62c1ebba8f5d5a40565
Jonathan Kyule <jonathankyule2@gmail.com>
2023-03-17 15:57:05 -0700

    lower to upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(c);
+			d++;
+		}
+	return (0);
+
+}
