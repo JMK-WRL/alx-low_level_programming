e515ed60c5308a1be03ed976fb2a282cc2cd77dd
Jonathan Kyule <jonathankyule2@gmail.com>
2023-03-20 12:33:30 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Return: 1 if success
+ * Return: 1 (success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that place of art is useful\" - Dora Korpar,2015-10-19\n", stdout);
 	return (1);
 }
