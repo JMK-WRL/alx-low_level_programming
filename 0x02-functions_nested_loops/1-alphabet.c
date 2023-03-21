101a93bc75980f103e833b7981d7771676e08244
Jonathan Kyule <jonathankyule2@gmail.com>
2023-03-21 10:55:35 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabets
+ *
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
