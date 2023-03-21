c4e9ba2cffa19e1ce24d79415db8fd7115bbd2d6
Jonathan Kyule <jonathankyule2@gmail.com>
2023-03-21 16:33:14 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @x: int to be added to y
+ * @y: int to be added to x
+ * Return: sum of x and z
+ */
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
