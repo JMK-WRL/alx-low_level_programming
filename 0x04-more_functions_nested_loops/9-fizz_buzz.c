5ec87fa6482c5cf70a1e7efad5a33ba60c324633
Jonathan Kyule <jonathankyule2@gmail.com>
2023-03-23 10:46:49 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints a fizz buzz program
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+	if ((a % 3 == 0) && (a % 5 == 0))
+	{
+		printf("FizzBuzz");
+	}
+	else if (a % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	else if (a % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else
+	{
+		printf("%d", a);
+	}
+	if (a != 100)
+	{
+		printf(" ");
+	}
+	}
+	printf("\n");
+	return (0);
+}
