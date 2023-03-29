fdf4215d6e82dae19ff902886f05b48a429bed5f
Jonathan Kyule <jonathankyule2@gmail.com>
2023-03-29 13:55:25 -0700

    leet code

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *leet - function that encode a string into 1337
+ * @a: Input value
+ * Return: a
+ */
+
+char *leet(char *a)
+{
+	int x;
+	int y;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "443300771l";
+
+	x = 0;
+	y = 0;
+
+	for (x = 0; a[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (a[x] == s1[y])
+			{
+				a[x] = s2[y];
+			}
+		}
+	}
+	return (a);
+}
