62edab4e51a2679d466c0c3c5f801abf01a9b868
Jonathan Kyule <jonathankyule2@gmail.com>
2023-03-30 13:25:37 -0700

    print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -22,3 +22,28 @@ void print_buffer(char *b, int size)
 	{
 		j = size - o < 10 ? size - o : 10;
 		printf("%08x:", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf(" ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
+}
