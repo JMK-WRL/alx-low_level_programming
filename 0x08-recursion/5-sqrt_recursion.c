ea2749c5b4a862aaee1fad0221fcfa00b401b06b
Jonathan Kyule <jonathankyule2@gmail.com>
2023-04-05 11:32:12 -0700

    squareroot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - functions that returns the natural square root of a number
+ * @n: number to return sqr
+ * Return: The resulting square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+
+	}
+	return (actual_sqrt_recursion(n,0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses again to find sqrt
+ * @n: number to be checked
+ * @i: iterator
+ * Return: resulting square root.
+ */
+
+int actual_sqrt_recursion(int n, int i)
+{
+	
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (actual_sqrt_recursion(n, i + 1));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,4 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function that returns a prime number
