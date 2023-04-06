be6cf0990f5d4477981d7e3de614e00776eb0e47
Jonathan Kyule <jonathankyule2@gmail.com>
2023-04-06 08:20:34 -0700

    delete

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,23 @@
 #include "main.h"
 
 /**
- * is_prime_number - function that returns a prime number
+ * is_prime_number - function that whether a number is an prime number
+ * @n: number being checked
+ * Return: 1 (success)
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	for (int i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
