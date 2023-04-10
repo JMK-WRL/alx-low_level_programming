ea0248cbf55011c715758799574b6151845e22ba
Jonathan Kyule <jonathankyule2@gmail.com>
2023-04-10 12:33:05 -0700

    new name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that prints the name of the file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
