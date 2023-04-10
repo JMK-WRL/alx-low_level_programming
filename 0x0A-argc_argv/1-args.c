e936a3445153c0d066f189e4d240bb3e0a823247
Jonathan Kyule <jonathankyule2@gmail.com>
2023-04-10 13:43:53 -0700

    nargs

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(int argc, char *argv)
+
+/**
+ * main - functioon that prints number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (sucess)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	(void) argv;
+
+	if (argc > 0)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%d\n", argc - 1);
+		}
+		}
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,1 +1,7 @@
-int main(int argc, char* argv[]);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+
+#endif
