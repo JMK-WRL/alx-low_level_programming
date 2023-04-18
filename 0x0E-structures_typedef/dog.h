b04bd759af839ae946a39f42cebcf0f96f2aed56
Jonathan Kyule <jonathankyule2@gmail.com>
2023-04-18 03:27:19 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - info about a dog
+ * @name: First element
+ * @age: Second element
+ * @owner: Third element
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
