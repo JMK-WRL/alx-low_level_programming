d72c1344d7c7c8465a39ccb28c662fc620a57556
Jonathan Kyule <jonathankyule2@gmail.com>
2023-04-19 12:49:55 -0700

    a goal is meant to be reached

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that calls on the operators
+ * @argv: array of pointers
+ * @argc: number of arguments
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*oprt)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	oprt = get_op_func(argv[2]);
+
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - returns the sum of a and b
+ * @a: value
+ * @b: value
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of a and b
+ * @a: value
+ * @b: value
+ * Return: diff of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns products of a and b
+ * @a: value
+ * @b: value
+ * Returns: prod of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: value
+ * @b: value 
+ * Return: div of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus
+ * @a: value
+ * @b: value
+ * Return: mod of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
