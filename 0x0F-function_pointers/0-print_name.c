c54cb0a78be06529b0276b021e0ee024dc1baba6
Jonathan Kyule <jonathankyule2@gmail.com>
2023-04-19 13:41:49 -0700

    corrections

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
  * print_name - function that prints a name
  * @name: name to print
  * @f: the pointer to a function
- * Return 0 (success)
+ * Return: 0 (success)
  */
 
 void print_name(char *name, void (*f)(char *))
