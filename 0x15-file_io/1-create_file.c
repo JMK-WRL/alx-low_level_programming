0787c0144c9a717f82ca395f4027e4d31e176fe7
Jonathan Kyule <jonathankyule2@gmail.com>
2023-05-08 11:17:34 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,20 +14,21 @@ int create_file(const char *filename, char *text_content)
 	int w;
 
 	str = 0;
+
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		for (str = 0, text_content[str];)
+		for (str = 0; text_content[str];)
 			str++;
 	}
 
 	File = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(File, text_content, str);
 
-	if (File == -1, w == -1)
+	if (File == -1 || w == -1)
 	{
 		return (-1);
 	}
