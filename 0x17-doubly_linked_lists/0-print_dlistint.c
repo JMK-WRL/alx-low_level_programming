43e28eeb09309b893827b52ca723c24ef0c716fe
Jonathan <jonathankyule2@gmail.com>
2023-08-18 01:34:55 -0700

    print list betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,24 +1,22 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list.
- * @h: Pointer to the head node of the doubly linked list.
- * Return: The number of nodes in the list.
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to the head node of list
+ * Return: The number of the node in the list
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    size_t count = 0;
+	const dlistint_t *current = h;
+	size_t count = 0;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-        count++;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
 
-    return count;
+	return (count);
 }
-
