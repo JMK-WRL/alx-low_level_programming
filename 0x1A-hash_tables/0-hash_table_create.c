98d165b22ef8f581186f3f7d85e344315da9cc0d
Jonathan <jonathankyule2@gmail.com>
2023-09-08 14:02:24 -0700

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,9 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (hash_table_created == NULL)
 		return (NULL);
-	
 	hash_table_created->size = size;
-	
 	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (hash_table_created->array == NULL)
