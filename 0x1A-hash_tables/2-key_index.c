41bfb34f096fdcac58e01c7fa98e62f25c21415e
Jonathan <jonathankyule2@gmail.com>
2023-09-08 14:08:34 -0700

    task 2key index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long in hash_value;
+	unsigned long int hash_value;
 
 	if (key == NULL || size == 0)
 		return (0);
