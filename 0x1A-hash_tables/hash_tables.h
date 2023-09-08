dede93f3bb788fb3f1fe33c4ff8410e4b4c55824
Jonathan <jonathankyule2@gmail.com>
2023-09-08 13:36:35 -0700

    djb2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -31,6 +31,7 @@ typedef struct hash_table_s
      hash_node_t **array;
 } hash_table_t;
 
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)i;
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif
