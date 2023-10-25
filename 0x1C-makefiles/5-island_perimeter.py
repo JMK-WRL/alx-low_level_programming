39b1b2e751839fccdd2a7852606c1af402246fad
Jonathan <jonathankyule2@gmail.com>
2023-10-25 03:51:53 -0700

    code style

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Function that defines an island perimeter measuring function"""
 
+
 def island_perimeter(grid):
     """
     Returns the perimeter of an island
@@ -19,8 +20,8 @@ def island_perimeter(grid):
         for j in range(width):
             if grid[i][j] == 1:
                 size += 1
-                if (j > 0 and grid[i] [j - 1] == 1):
+                if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
-                if (i > 0 and grid[i - 1] [j] == 1):
+                if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
     return size * 4 - edges * 2
