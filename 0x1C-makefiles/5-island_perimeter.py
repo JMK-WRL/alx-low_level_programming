88536311cc27dd3a2723d99e2d45589541511645
Jonathan <jonathankyule2@gmail.com>
2023-10-25 03:48:24 -0700

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Function that defines an island perimeter measuring function"""
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of an island
+
+    Args:
+        grid (list): a list of list of integers
+
+    """
+
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i] [j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1] [j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
